2553c71db059f1d59c0f22f8faf0ccceb4765b6a
codeoD <760206335@qq.com>
2022-05-23 18:26:50 +0800

    调整配置

--- ui/vite.config.js
+++ ui/vite.config.js
@@ -1,15 +1,25 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
+const htmlPlugin = () => {
+  return {
+    name: "html-transform",
+    transformIndexHtml(html) {
+      return html.replace(/<title>(.*?)<\/title>/, `<title>I Sound</title>`);
+    },
+  };
+};
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), htmlPlugin()],
   base: "./",
   envDir: "./",
   build: {
     outDir: "../renderer",
     emptyOutDir: true,
-    rollupOptions: {},
+    rollupOptions: {
+      // plugins: [],
+    },
   },
-  // publicDir: "../renderer/assets",
 });
